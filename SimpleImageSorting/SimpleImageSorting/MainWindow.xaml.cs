5ea0c3dc32f74a7d92a419986cd96d84a2b60007
Jo G <jo2012@online.de>
2017-06-14 14:53:28 +0200

    done with static path

--- SimpleImageSorting/SimpleImageSorting/MainWindow.xaml.cs
+++ SimpleImageSorting/SimpleImageSorting/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +22,100 @@ namespace SimpleImageSorting
     /// </summary>
     public partial class MainWindow : Window
     {
+        String folder = @"E:\Data\01 Bilder\2017\!Handy\";
+        String subFolder;
+        string[] files;
+        int imgCounter = 0;
+        Pic currentPic;
+        Pic previousPic;
+        bool deleteFile;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.KeyDown += new KeyEventHandler(MainWindow_KeyDown);
+            files = Directory.GetFiles(folder, "*.jpg", SearchOption.TopDirectoryOnly);
+        }
+
+        private void image_Loaded(object sender, RoutedEventArgs e)
+        {
+            openNextImage();
+        }
+
+        private void openNextImage()
+        {
+            previousPic = currentPic;
+            currentPic = new Pic(new FileInfo(files[imgCounter]));
+
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(files[imgCounter]);
+            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+            bitmap.EndInit();
+
+            image.Source = bitmap;
+
+            imgCounter++;
+        }
+
+        private void storePreviousImage()
+        {
+            if (previousPic == null)
+                return;
+
+            if (deleteFile)
+            {
+                deleteFile = false;
+                File.Delete(previousPic.PathAndName);
+                return;
+            }
+
+            Directory.CreateDirectory(folder + subFolder);
+
+            File.Move(previousPic.PathAndName, folder + subFolder + @"\" + previousPic.Date + ".jpg");
+        }
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.D1:
+                    subFolder = "1";
+                    break;
+                case Key.D2:
+                    subFolder = "2";
+                    break;
+                case Key.D3:
+                    subFolder = "3";
+                    break;
+                case Key.D4:
+                    subFolder = "4";
+                    break;
+                case Key.D5:
+                    subFolder = "5";
+                    break;
+                case Key.D6:
+                    subFolder = "6";
+                    break;
+                case Key.D7:
+                    subFolder = "7";
+                    break;
+                case Key.D8:
+                    subFolder = "8";
+                    break;
+                case Key.D9:
+                    subFolder = "9";
+                    break;
+                case Key.D0:
+                    subFolder = "0";
+                    break;
+                case Key.Delete:
+                    deleteFile = true;
+                    break;
+            }
+
+            openNextImage();
+            storePreviousImage();
         }
     }
 }
---
+++ SimpleImageSorting/SimpleImageSorting/Pic.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace SimpleImageSorting
+{
+    class Pic
+    {
+        public String Name { get; private set; }
+        public String PathAndName { get; private set; }
+        public String Date { get; private set; }
+
+        public Pic(FileInfo fileInfo)
+        {
+            Name = fileInfo.Name;
+            PathAndName = fileInfo.FullName;
+            Date = getDate(fileInfo);
+        }
+
+        private string getDate(FileInfo f)
+        {
+            using (FileStream fs = new FileStream(f.FullName, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                BitmapSource img = BitmapFrame.Create(fs);
+                BitmapMetadata md = (BitmapMetadata)img.Metadata;
+                char[] dateAsChar = md.DateTaken.ToCharArray();
+                char[] formattedDateAsChar = { dateAsChar[6], dateAsChar[7], dateAsChar[8], dateAsChar[9], '-', dateAsChar[3], dateAsChar[4], '-', dateAsChar[0], dateAsChar[1], '_', dateAsChar[11], dateAsChar[12], '-', dateAsChar[14], dateAsChar[15], '-', dateAsChar[17], dateAsChar[18] };
+                return new string(formattedDateAsChar);
+            }
+        }
+    }
+}
